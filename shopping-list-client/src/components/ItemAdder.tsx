0dd652212e87157e7f364e18da0b8e0b2bbfb5cd
Malka <malkabrodsky556@gmail.com>
2024-07-31 02:04:45 +0300

    Display categories.

--- shopping-list-client/src/components/ItemAdder.tsx
+++ shopping-list-client/src/components/ItemAdder.tsx
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
 import { Button, Input, Select } from 'antd';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../store';
 
 const { Option } = Select;
 
 export const ItemAdder: React.FC = () => {
-    const [inputValue, setInputValue] = useState<string>('');
-    const [selectedOption, setSelectedOption] = useState<string | undefined>(undefined);
+    const [ inputValue, setInputValue ] = useState<string>( '' );
+    const [ selectedOption, setSelectedOption ] = useState<string | undefined>( undefined );
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value);
+    const categories = useSelector( ( state: RootState ) => state.categories.categories );
+
+    const handleInputChange = ( e: React.ChangeEvent<HTMLInputElement> ) => {
+        setInputValue( e.target.value );
     };
 
-    const handleSelectChange = (value: string) => {
-        setSelectedOption(value);
+    const handleSelectChange = ( value: string ) => {
+        setSelectedOption( value );
     };
 
     return (
-        <div style={{ display: 'flex', gap: '10px', padding: '1rem', justifyContent: 'center' }}>
+        <div style={ { display: 'flex', gap: '10px', padding: '1rem', justifyContent: 'center' } }>
             <Input
-                value={inputValue}
-                onChange={handleInputChange}
+                value={ inputValue }
+                onChange={ handleInputChange }
                 placeholder="הכנס מוצר חדש"
-                style={{ width: '300px' }}
+                style={ { width: '300px' } }
             />
-            <Button>הוסף</Button>
             <Select
-                value={selectedOption}
-                onChange={handleSelectChange}
+                value={ selectedOption }
+                onChange={ handleSelectChange }
                 placeholder="בחר קטגוריה"
-                style={{ width: '300px' }}
+                style={ { width: '300px' } }
             >
-                <Option value="Option 1">Option 1</Option>
-                <Option value="Option 2">Option 2</Option>
-                <Option value="Option 3">Option 3</Option>
-                <Option value="Option 4">Option 4</Option>
-                <Option value="Option 5">Option 5</Option>
+                { categories.map( ( category ) => <Option value={ category.key }>{ category.key }</Option> ) }
             </Select>
+            <Button>הוסף</Button>
         </div>
     );
 };
