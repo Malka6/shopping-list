ff7e3e2245b62a63d5624872faeec70fcbe4a6b2
Malka <malkabrodsky556@gmail.com>
2024-07-31 01:43:57 +0300

    Add api consts.

---
+++ shopping-list-client/src/consts/api.ts
@@ -0,0 +1,6 @@
+export const api = {
+    baseUrl: "http://localhost:3001/api",
+    getAllCategoriesRoute: "categories/all",
+    addProductRoute: "products/add",
+    getStoreInventoryRoute: 'products/inventory'
+}
