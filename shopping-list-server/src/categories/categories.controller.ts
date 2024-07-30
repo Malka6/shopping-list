6492e54eb6f1409fa8902a11d09e6654daef24df
Malka <malkabrodsky556@gmail.com>
2024-07-31 00:06:46 +0300

    Add try/catch.

--- shopping-list-server/src/categories/categories.controller.ts
+++ shopping-list-server/src/categories/categories.controller.ts
@@ -5,11 +5,16 @@ export class CategoriesController {
     constructor ( private categoriesService: CategoriesService ) { }
 
     getAllCategories = async ( _req: Request, res: Response ) => {
-        const categories = await this.categoriesService.getUniqueCategories();
-        if ( !categories || categories.length === 0 ) {
+        try {
+            const categories = await this.categoriesService.getUniqueCategories();
+            if ( !categories || categories.length === 0 ) {
+                return res.status( 500 ).json( { error: 'Failed to get categories list' } );
+            }
+
+            return res.status( 200 ).json( { categories } )
+        } catch ( error ) {
+            console.log( error );
             return res.status( 500 ).json( { error: 'Failed to get categories list' } );
         }
-
-        return res.status( 200 ).json( { categories } )
     }
 }
--- shopping-list-server/src/products/products.controller.ts
+++ shopping-list-server/src/products/products.controller.ts
@@ -12,19 +12,29 @@ export class ProductsController {
             return res.status( 400 ).json( { error: 'Name and category are required' } );
         }
 
-        const productId = await this.productsService.insertProduct( product );
-        if ( productId ) {
-            return res.status( 201 ).json( { id: productId } );
-        } else {
-            return res.status( 500 ).json( { error: 'Failed to insert product' } );
+        try {
+            const productId = await this.productsService.insertProduct( product );
+            if ( productId ) {
+                return res.status( 201 ).json( { id: productId } );
+            } else {
+                return res.status( 500 ).json( { error: 'Failed to insert product' } );
+            }
+        } catch ( error ) {
+            console.log( error );
+            return res.status( 500 ).json( { error: 'Failed to get categories list' } );
         }
     };
 
     getInventory = async ( req: Request, res: Response ) => {
-        const inventory: Inventory = await this.productsService.getStoreInventory();
-        if ( !inventory ) {
-            return res.status( 500 ).json( { error: 'Failed to get products inventory.' } )
-        } else return res.status( 200 ).json( { inventory } )
+        try {
+            const inventory: Inventory = await this.productsService.getStoreInventory();
+            if ( !inventory ) {
+                return res.status( 500 ).json( { error: 'Failed to get products inventory.' } )
+            } else return res.status( 200 ).json( { inventory } )
+        } catch ( error ) {
+            console.log( error );
+            return res.status( 500 ).json( { error: 'Failed to get categories list' } );
+        }
     }
 }
 
